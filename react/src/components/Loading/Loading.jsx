0e9ba10676fcd447733e236fe26ab036f06387d6
pedrots <pedrotorres9843@gmail.com>
2025-09-28 14:45:15 -0300

    Mudança no loading

--- react/src/components/Loading/Loading.jsx
+++ react/src/components/Loading/Loading.jsx
@@ -22,7 +22,39 @@ export default function Loading({ active = false, text = "Carregando..." }) {
       aria-busy="true"
     >
       <div className={styles.box}>
-        <div className={styles.spinner} aria-hidden="true" />
+        <div className={styles.uiverseLoader} aria-hidden="true">
+          {/* Loader Uiverse */}
+          <div className="loader">
+            <div className="box box0">
+              <div></div>
+            </div>
+            <div className="box box1">
+              <div></div>
+            </div>
+            <div className="box box2">
+              <div></div>
+            </div>
+            <div className="box box3">
+              <div></div>
+            </div>
+            <div className="box box4">
+              <div></div>
+            </div>
+            <div className="box box5">
+              <div></div>
+            </div>
+            <div className="box box6">
+              <div></div>
+            </div>
+            <div className="box box7">
+              <div></div>
+            </div>
+            <div className="ground">
+              <div></div>
+            </div>
+          </div>
+        </div>
+
         <span className={styles.label}>{text}</span>
       </div>
     </div>,
