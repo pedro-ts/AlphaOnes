974bd971f678cea1a830b44ac155584237c10a2d
pedrots <pedrotorres9843@gmail.com>
2025-09-29 21:56:06 -0300

    Criação da base de dados que será recebido pela tela principal sem aplicação de filtros

--- react/src/views/Estatisticas/Estatisticas.jsx
+++ react/src/views/Estatisticas/Estatisticas.jsx
@@ -4,12 +4,88 @@ import { useState } from 'react'
 import styles from './Estatisticas.module.css'
 
 const Estatisticas = () => {
-  const [bases, setBases] = useState([
-    "governo",
-    "numeros-privados",
-    "aposentados",
-    "sao-paulo",
-  ]);
+  const [bases, setBases] = useState([]);
+  setBases(["governo", "numeros-privados", "aposentados", "sao-paulo"]);
+
+  const [mainInfos, setMainInfos] = useState([]);
+  setBases(
+    [
+      ["Taxa de completamento geral", "39%"],
+      [
+        { dia: "01/10", valor: 120 },
+        { dia: "02/10", valor: 80 },
+        { dia: "03/10", valor: 150 },
+        { dia: "04/10", valor: 200 },
+        { dia: "05/10", valor: 90 },
+        { dia: "06/10", valor: 300 },
+        { dia: "07/10", valor: 250 },
+        { dia: "08/10", valor: 170 },
+        { dia: "09/10", valor: 220 },
+        { dia: "10/10", valor: 140 },
+      ],
+    ], //fim da taxa de completamento
+    [
+      ["Ligações feitas no dia", "200.004"],
+      [
+        { dia: "01/10", valor: 120 },
+        { dia: "02/10", valor: 80 },
+        { dia: "03/10", valor: 150 },
+        { dia: "04/10", valor: 200 },
+        { dia: "05/10", valor: 90 },
+        { dia: "06/10", valor: 300 },
+        { dia: "07/10", valor: 250 },
+        { dia: "08/10", valor: 170 },
+        { dia: "09/10", valor: 220 },
+        { dia: "10/10", valor: 140 },
+      ],
+    ], //fim das ligações feitas no dia
+    [
+      ["Falhas no dia", "130.012"],
+      [
+        { dia: "01/10", valor: 120 },
+        { dia: "02/10", valor: 80 },
+        { dia: "03/10", valor: 150 },
+        { dia: "04/10", valor: 200 },
+        { dia: "05/10", valor: 90 },
+        { dia: "06/10", valor: 300 },
+        { dia: "07/10", valor: 250 },
+        { dia: "08/10", valor: 170 },
+        { dia: "09/10", valor: 220 },
+        { dia: "10/10", valor: 140 },
+      ],
+    ], //fim das falhas no dia
+    [
+      ["Caixas postais", "212"],
+      [
+        { dia: "01/10", valor: 120 },
+        { dia: "02/10", valor: 80 },
+        { dia: "03/10", valor: 150 },
+        { dia: "04/10", valor: 200 },
+        { dia: "05/10", valor: 90 },
+        { dia: "06/10", valor: 300 },
+        { dia: "07/10", valor: 250 },
+        { dia: "08/10", valor: 170 },
+        { dia: "09/10", valor: 220 },
+        { dia: "10/10", valor: 140 },
+      ],
+    ], //fim das caixas postais
+    [
+      ["Custo telefonia diário", "R$2.300"],
+      [
+        { dia: "01/10", valor: 120 },
+        { dia: "02/10", valor: 80 },
+        { dia: "03/10", valor: 150 },
+        { dia: "04/10", valor: 200 },
+        { dia: "05/10", valor: 90 },
+        { dia: "06/10", valor: 300 },
+        { dia: "07/10", valor: 250 },
+        { dia: "08/10", valor: 170 },
+        { dia: "09/10", valor: 220 },
+        { dia: "10/10", valor: 140 },
+      ], //fim do custo telefonia diário
+    ]
+    // [["Custo telefonia diário", "R$2.300"], []]
+  );
 
   
   return (
