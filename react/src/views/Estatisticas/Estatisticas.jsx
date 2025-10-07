e008d12f3349d8900a69a9646a459c453e89a90b
pedrots <pedrotorres9843@gmail.com>
2025-10-07 15:39:03 -0300

    Ajuste realizado na tela inicial das estatisticas

--- react/src/views/Estatisticas/Estatisticas.jsx
+++ react/src/views/Estatisticas/Estatisticas.jsx
@@ -2,8 +2,6 @@ import FiltroEstatisticas from "../../components/FiltroEstatisticas/FiltroEstati
 import { useState } from "react";
 // components
 import styles from "./Estatisticas.module.css";
-import GraficoColunasEstatisticas from "../../components/GraficoColunasEstatisticas/GraficoColunasEstatisticas";
-import EstatisticaMainInfoConatiner from "../../components/EstatisticaMainInfoConatiner/EstatisticaMainInfoConatiner";
 
 const Estatisticas = () => {
   const [bases, setBases] = useState([
@@ -12,119 +10,74 @@ const Estatisticas = () => {
     "aposentados",
     "sao-paulo",
   ]);
-
-  const [mainInfos] = useState([
-    {
-      titulo: "Taxa de completamento geral",
-      valor: "39%",
-      dias: [
-        "01/10",
-        "02/10",
-        "03/10",
-        "04/10",
-        "05/10",
-        "06/10",
-        "07/10",
-        "08/10",
-        "09/10",
-        "10/10",
-      ],
-      valores: [120, 80, 150, 200, 90, 300, 250, 170, 220, 140],
-    },
-    {
-      titulo: "Ligações feitas no dia",
-      valor: "200.004",
-      dias: [
-        "01/10",
-        "02/10",
-        "03/10",
-        "04/10",
-        "05/10",
-        "06/10",
-        "07/10",
-        "08/10",
-        "09/10",
-        "10/10",
-      ],
-      valores: [120, 80, 150, 200, 90, 300, 250, 170, 220, 140],
-    },
-    {
-      titulo: "Falhas no dia",
-      valor: "130.012",
-      dias: [
-        "01/10",
-        "02/10",
-        "03/10",
-        "04/10",
-        "05/10",
-        "06/10",
-        "07/10",
-        "08/10",
-        "09/10",
-        "10/10",
-      ],
-      valores: [120, 80, 150, 200, 90, 300, 250, 170, 220, 140],
-    },
-    {
-      titulo: "Caixas postais",
-      valor: "212",
-      dias: [
-        "01/10",
-        "02/10",
-        "03/10",
-        "04/10",
-        "05/10",
-        "06/10",
-        "07/10",
-        "08/10",
-        "09/10",
-        "10/10",
-      ],
-      valores: [120, 80, 150, 200, 90, 300, 250, 170, 220, 140],
-    },
-    {
-      titulo: "Custo telefonia diário",
-      valor: "R$ 2.300",
-      dias: [
-        "01/10",
-        "02/10",
-        "03/10",
-        "04/10",
-        "05/10",
-        "06/10",
-        "07/10",
-        "08/10",
-        "09/10",
-        "10/10",
-      ],
-      valores: [120, 80, 150, 200, 90, 300, 250, 170, 220, 140],
-    },
-  ]);
-  // const [mainInfos] = useState([
-  //   {
-  //     titulo: "Taxa de completamento geral",
-  //     valor: "39%",
-  //     dias: ["01/10","02/10","03/10","04/10","05/10","06/10","07/10","08/10","09/10","10/10"],
-  //     valores: [120,80,150,200,90,300,250,170,220,140],
-  //   }
-  // ]);
+  const [searched, setSearched] = useState(false);
 
   return (
     <div className={styles["estatisticas-container"]}>
       <FiltroEstatisticas bases={bases} />
       {/* duas divs, sendo uma par ao principal quando não se tem nenhum filtro aplicada e outra para quando temos o filtro ativado, para isso é necessario criar 2 componentes para cada uma das situações */}
-      <div className={styles["main-container"]}>
-        {mainInfos.map((info, i) => (
-          <EstatisticaMainInfoConatiner
-            key={i}
-            titulo={info.titulo}
-            valor={info.valor}
-            dias={info.dias}
-            valores={info.valores}
-          />
-        ))}
-        ;
-      </div>
+      {searched === true && (
+        <div className={styles["main-container-searched"]}></div>
+      )}
+
+      {searched == false && (
+        <div className={styles["main-container"]}>
+          <div className={styles["box-of-star1"]}>
+            <div className={`${styles.star} ${styles["star-position1"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position2"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position3"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position4"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position5"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position6"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position7"]}`}></div>
+          </div>
+
+          <div className={styles["box-of-star2"]}>
+            <div className={`${styles.star} ${styles["star-position1"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position2"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position3"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position4"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position5"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position6"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position7"]}`}></div>
+          </div>
+
+          <div className={styles["box-of-star3"]}>
+            <div className={`${styles.star} ${styles["star-position1"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position2"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position3"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position4"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position5"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position6"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position7"]}`}></div>
+          </div>
+
+          <div className={styles["box-of-star4"]}>
+            <div className={`${styles.star} ${styles["star-position1"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position2"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position3"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position4"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position5"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position6"]}`}></div>
+            <div className={`${styles.star} ${styles["star-position7"]}`}></div>
+          </div>
+
+          <div data-js="astro" className={styles.astronaut}>
+            <div className={styles.head}></div>
+            <div className={`${styles.arm} ${styles["arm-left"]}`}></div>
+            <div className={`${styles.arm} ${styles["arm-right"]}`}></div>
+            <div className={styles.body}>
+              <div className={styles.panel}></div>
+            </div>
+            <div className={`${styles.leg} ${styles["leg-left"]}`}></div>
+            <div className={`${styles.leg} ${styles["leg-right"]}`}></div>
+            <div className={styles.schoolbag}></div>
+          </div>
+          <div className={styles["text-container"]}>
+            <h2 className={styles["title-main"]}>Aguardando pesquisa...</h2>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
